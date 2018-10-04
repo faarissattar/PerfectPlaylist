c394b10f02d5e1ff3bc2661182cde758bc364c54
Emanuel Urbina <emanuelurbina@live.com>
2018-10-03 20:47:14 -0500

    spotify api base

--- app/src/main/java/com/example/faari/perfectplaylsit/MainActivity.java
+++ app/src/main/java/com/example/faari/perfectplaylsit/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.faari.perfectplaylsit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private static final String CLIENT_ID = "ffe427ae0c784377ab9f5afc7bf47a15";
+    private static final String REDIRECT_URI = "https://example.com/redirect/";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    @Override   //  question over needing to explicitly create an overriden function
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onStart(){
+        //  super calls the onStart function of the superType for this class
+        super.onStart();
+
+        ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID)
+                                                                .setRedirectUri(REDIRECT_URI)
+                                                                .showAuthView(true)
+                                                                .build();
+
+        SpotifyAppRemote.CONNECTOR.connect(this, connectionParams, new Connector.ConnectionListener() {
+            @Override
+            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                mSpotifyAppRemote = spotifyAppRemote;
+                Log.d("MainActivity", "Connected! Yay!");
+
+
+                connected();
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                Log.e("MainActivity", throwable.getMessage(), throwable);
+
+            }
+        });
+    }
+
+    private void connected(){
+        mSpotifyAppRemote.getPlayerApi().play("spotify:user:spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        SpotifyAppRemote.CONNECTOR.disconnect(mSpotifyAppRemote);
     }
 }
