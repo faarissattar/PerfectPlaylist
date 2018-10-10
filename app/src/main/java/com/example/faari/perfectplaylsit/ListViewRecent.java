b13f08da993055e578baa81ce13bd800461f0f8e
Caitlin Tibbetts <caitlin@tibbettsfamily.com>
2018-10-10 15:25:13 -0500

    Created a layout for a song in the recently played list and quick Adapter to go with it that can be changed to our needs as they arise.

---
+++ app/src/main/java/com/example/faari/perfectplaylsit/ListViewRecent.java
@@ -0,0 +1,60 @@
+package com.example.faari.perfectplaylsit;
+
+import android.app.Activity;
+
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ListViewRecent extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] title;
+    private final String[] artist;
+    private final String[] album;
+    private final Integer[] imgid;
+
+    public ListViewRecent(Activity context, String[] title,String[] artist, String[] album, Integer[] imgid) {
+        super(context, R.layout.list_view_recent, title);
+
+        this.context=context;
+        this.title =title;
+        this.artist =artist;
+        this.album =album;
+        this.imgid=imgid;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        ViewHolder holder;
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.list_view_recent, null);
+            holder = new ViewHolder();
+            holder.titleText = (TextView) convertView.findViewById(R.id.title);
+            holder.artistText = (TextView) convertView.findViewById(R.id.artist);
+            holder.albumText = (TextView) convertView.findViewById(R.id.album);
+            holder.albumImage = (ImageView) convertView.findViewById(R.id.icon);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        return convertView;
+    };
+
+    static class ViewHolder {
+        private TextView titleText;
+        private TextView artistText;
+        private TextView albumText;
+        private ImageView albumImage;
+    }
+}
